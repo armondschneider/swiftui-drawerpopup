d152116796d184372bd5a29ac350a9f59c3fd89f
armondschneider <mondoloves@icloud.com>
2024-04-10 13:34:14 -0700

    first commit

--- drawerpopup/ContentView.swift
+++ drawerpopup/ContentView.swift
@@ -2,23 +2,24 @@
 //  ContentView.swift
 //  drawerpopup
 //
-//  Created by Armond Schneider on 4/9/24.
+//  Created by Armond Schneider (@armondme) on 4/8/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isDrawerOpen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Text("@armondschneider")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+

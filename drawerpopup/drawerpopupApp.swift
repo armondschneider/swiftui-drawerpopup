99edf237177e20c5c1957e3fde0832f1c06abc98
armondschneider <mondoloves@icloud.com>
2024-04-09 20:43:19 -0700

    Initial Commit

---
+++ drawerpopup/drawerpopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawerpopupApp.swift
+//  drawerpopup
+//
+//  Created by Armond Schneider on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct drawerpopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
